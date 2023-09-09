51e43fd2d4960a5daeb319506b82f2a8f74c22f2
James Roman <platinio94@gmail.com>
2023-09-09 11:29:24 -0300

    update code naming conventions

--- Sample/Scripts/ShopDatabaseExample.cs
+++ Sample/Scripts/ShopDatabaseExample.cs
@@ -5,11 +5,11 @@ namespace ScriptableObjectDatabase.Sample
     public class ShopDatabaseExample : MonoBehaviour
     { 
         [SerializeField] private ShopItemsDatabase shopItemsDB;
-        [SerializeField] private uint m_id;
+        [SerializeField] private uint id;
 
         private void Start()
         {
-            var item = shopItemsDB.GetItem(m_id);
+            var item = shopItemsDB.GetItem(id);
             Debug.Log($"Item Name: {item.name}");
         }
     }
