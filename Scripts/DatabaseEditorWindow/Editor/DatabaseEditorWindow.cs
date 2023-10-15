672ed0e850057c68b7541ebb9dcc4fee3651f5ff
James Roman <platinio94@gmail.com>
2023-10-15 16:21:50 -0300

    create base scripts to create an editor to edi database items

---
+++ Scripts/DatabaseEditorWindow/Editor/DatabaseEditorWindow.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ScriptableObjectDatabase
+{
+    public abstract class DatabaseEditorWindow<Database, Entry> : EditorWindow 
+        where Database : ScriptableDatabase<Entry> 
+        where Entry : ScriptableItem
+    {
+        [SerializeField] protected VisualTreeAsset visualTreeAsset = default;
+        [SerializeField] protected VisualTreeAsset listElementTreeAsset;
+       
+        public virtual void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // Instantiate UXML
+            VisualElement labelFromUXML = visualTreeAsset.Instantiate();
+            root.Add(labelFromUXML);
+
+            CreateDatabaseListGUI(root);
+        }
+
+        protected void CreateDatabaseListGUI(VisualElement root)
+        {
+            var database = ScriptableDatabaseLoader.LoadDatabase(typeof(Database)) as Database;
+            IEnumerable<Entry> items = database.Items;
+
+            items = FilterEntries(items);
+            
+            var listView = root.Q("SkillListView") as ListView;
+            listView.Clear();
+            listView.makeItem = MakeSkillItem;
+            listView.bindItem = BindEntryItem;
+            listView.itemsSource = (IList)items;
+            listView.selectionType = SelectionType.Single;
+            listView.selectionChanged += OnEntrySelectionChanged;
+        }
+
+        protected virtual IEnumerable<Entry> FilterEntries(IEnumerable<Entry> entries) => entries;
+
+        private void OnEntrySelectionChanged(IEnumerable<object> selection)
+        {
+            var entry = selection.FirstOrDefault() as Entry;
+            var databaseEditor = rootVisualElement.Q<VisualElement>("SkillEditor");
+            
+            databaseEditor.Bind(new SerializedObject(entry));
+        }
+
+        private void BindEntryItem(VisualElement element, int index)
+        {
+            var database = ScriptableDatabaseLoader.LoadDatabase(typeof(Database)) as Database;
+            IEnumerable<Entry> items = database.Items;
+            
+            items = FilterEntries(items);
+            
+            var item = items.ToArray()[index];
+            
+            element.Q<Label>().text = item.Name;
+            element.Q("Icon").style.backgroundImage = new StyleBackground(GetEntryIcon(item));
+        }
+
+        protected abstract Sprite GetEntryIcon(Entry entry);
+        
+        private VisualElement MakeSkillItem() => listElementTreeAsset.CloneTree();
+    }
+}
---
+++ Scripts/DatabaseEditorWindow/Editor/DatabaseEditorWindowWithCategory.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ScriptableObjectDatabase
+{
+    public abstract class DatabaseEditorWindowWithCategory<Database, Entry, CategoryDatabase, CategoryEntry> : DatabaseEditorWindow<Database, Entry>
+        where Database : ScriptableDatabase<Entry> 
+        where Entry : ScriptableItem
+        where CategoryDatabase : ScriptableDatabase<CategoryEntry> 
+        where CategoryEntry : ScriptableItem
+    {
+        private static CategoryEntry SelectedCategory = null;
+        
+        public override void CreateGUI()
+        {
+            // Each editor window contains a root VisualElement object
+            VisualElement root = rootVisualElement;
+
+            // Instantiate UXML
+            VisualElement labelFromUXML = visualTreeAsset.Instantiate();
+            root.Add(labelFromUXML);
+
+            CreateCategoryListGUI(root);
+            CreateDatabaseListGUI(root);
+        }
+        
+        private void CreateCategoryListGUI(VisualElement root)
+        {
+            var categoryDatabase = ScriptableDatabaseLoader.LoadDatabase(typeof(CategoryDatabase)) as CategoryDatabase;
+
+            var listView = root.Q("SkillClassTypeListView") as ListView;
+            listView.Clear();
+            listView.makeItem = MakeCategoryTypeItem;
+            listView.bindItem = BindCategoryTypeItem;
+            listView.itemsSource = (IList)categoryDatabase.Items;
+            listView.selectionType = SelectionType.Single;
+            listView.selectionChanged += OnCategorySelectionChanged;
+        }
+        
+        private VisualElement MakeCategoryTypeItem() => listElementTreeAsset.CloneTree();
+        
+        private void BindCategoryTypeItem(VisualElement element, int index)
+        {
+            var categoryDatabase = ScriptableDatabaseLoader.LoadDatabase(typeof(CategoryDatabase)) as CategoryDatabase;
+            var item = categoryDatabase.Items.ToArray()[index];
+        
+            element.Q<Label>().text = item.Name;
+            element.Q("Icon").style.backgroundImage = new StyleBackground(GetCategoryIcon(item));
+        }
+        
+        private void OnCategorySelectionChanged(IEnumerable<object> selection)
+        {
+            SelectedCategory = selection.FirstOrDefault() as CategoryEntry;
+        
+            var skillDatabase = ScriptableDatabaseLoader.LoadDatabase(typeof(Database)) as Database;
+            IEnumerable<Entry> items = skillDatabase.Items;
+        
+            var listView = rootVisualElement.Q("SkillListView") as ListView;
+            items = FilterEntries(items);
+
+            listView.itemsSource = (IList)items;
+            listView.RefreshItems();
+        }
+
+        protected abstract Sprite GetCategoryIcon(CategoryEntry categoryEntry);
+
+    }
+
+}
+
