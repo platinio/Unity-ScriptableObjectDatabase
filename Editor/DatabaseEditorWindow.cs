8d02e3f5e7d1f6260274c466f724635120fb3fe3
James Roman <platinio94@gmail.com>
2026-07-17 16:18:03 -0400

    remove unused namespace

--- Editor/DatabaseEditorWindow.cs
+++ Editor/DatabaseEditorWindow.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEditor.Callbacks;
 using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.UIElements;
